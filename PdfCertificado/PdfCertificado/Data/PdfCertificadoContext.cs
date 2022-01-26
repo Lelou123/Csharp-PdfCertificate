4a65f97d9b4e9e041bc932a73e0efdb9a05a4306
Lelou123 <murillosama1@hotmail.com>
2022-01-26 17:39:47 -0300

    Register is working

---
+++ PdfCertificado/PdfCertificado/Data/PdfCertificadoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PdfCertificado.Models;
+
+namespace PdfCertificado.Data
+{
+    public class PdfCertificadoContext : DbContext
+    {
+        public PdfCertificadoContext (DbContextOptions<PdfCertificadoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PdfCertificado.Models.User> User { get; set; }
+    }
+}
---
+++ PdfCertificado/PdfCertificado/Migrations/20220126031458_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PdfCertificado.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Id");
+        }
+    }
+}
